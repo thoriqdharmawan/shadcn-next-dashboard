144c9899d4089031d14bd2e1b5de03e4b39adc5c
Thoriq Dharmawan <thoriq.dharmawan-guest@moladin.com>
2024-02-16 20:16:27 +0700

    feat(header): add header component

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,11 +1,19 @@
 import { Input } from "@/components/ui/input"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import Icon from "../icon"
 
 export default function Header() {
   return (
-    <header className="bg-gray-100 px-4 py-2 shadow-md">
-      <h1 className="text-lg font-semibold">
-        <Input />
-      </h1>
+    <header className="bg-gray-100 px-6 py-3 shadow-md flex gap-6 items-center">
+      <Input className="bg-white border-slate-300" placeholder="Search for anyhing..." />
+      <div className="flex gap-3 items-center">
+        <Icon name="messages" className="text-2xl text-gray-800" />
+        <Icon name="bell" className="text-2xl text-gray-800" />
+        <Avatar>
+          <AvatarImage src="https://github.com/shadcn.png" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+      </div>
     </header>
   )
 }
