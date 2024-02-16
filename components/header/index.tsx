64588d70fc63a9fe4aaad309f557bb16bc2c4dcf
Thoriq Dharmawan <thoriq.dharmawan-guest@moladin.com>
2024-02-16 23:40:25 +0700

    fix(header): minor style

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,10 +1,11 @@
 import { Input } from "@/components/ui/input"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+
 import Icon from "../icon"
 
 export default function Header() {
   return (
-    <header className="bg-white px-6 py-3 shadow-md flex gap-6 items-center">
+    <header className="bg-white py-4 px-9 shadow-md flex gap-6 items-center">
       <Input className="bg-white border-slate-300" placeholder="Search for anyhing..." />
       <div className="flex gap-3 items-center">
         <Icon name="messages" className="text-2xl text-gray-800" />
